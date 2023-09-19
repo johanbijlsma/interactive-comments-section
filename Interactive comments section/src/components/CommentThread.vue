e343485c6a266ab228ed65a1c2b6d52e3d4bf2f9
Johan Bijlsma <johan.bijlsma24@gmail.com>
2023-09-19 16:47:35 +0200

    updated the commentthread

--- Interactive comments section/src/components/CommentThread.vue
+++ Interactive comments section/src/components/CommentThread.vue
@@ -1,39 +1,98 @@
 <script setup lang="ts">
 import SingleComment from './SingleComment.vue'
 import commentsData from './../data.json'
+import { ref } from 'vue'
 defineProps<{
   msg: string
 }>()
+
+let CommentsLocal: any
+let loading = ref(false)
+
+const addCommentstoLocalStorage = () => {
+  loading.value = true
+  if (!localStorage.getItem('comments')) {
+    localStorage.setItem('comments', JSON.stringify(commentsData))
+    JSON.parse(localStorage.getItem('comments') as string)
+    loading.value = false
+  } else {
+    loading.value = false
+    return (CommentsLocal = JSON.parse(localStorage.getItem('comments') as string))
+  }
+}
+
+const updateCommentstoLocalStorage = () => {
+  if (localStorage.getItem('comments')) {
+    localStorage.setItem('comments', JSON.stringify(commentsData))
+    JSON.parse(localStorage.getItem('comments') as string)
+  } else {
+    localStorage.setItem('comments', JSON.stringify(commentsData))
+    JSON.parse(localStorage.getItem('comments') as string)
+  }
+}
+const newComment = ref({
+  content: '',
+  createdAt: new Date().toLocaleDateString(),
+  score: 0,
+  replyingTo: '',
+  user: commentsData.currentUser,
+  replies: [],
+  id: CommentsLocal ? CommentsLocal.comments.length + 1 : 348
+})
+
+const addComment = () => {
+  CommentsLocal.comments.push(newComment.value)
+  console.log(newComment.value)
+  updateCommentstoLocalStorage()
+  newComment.value.content = ''
+}
+
+addCommentstoLocalStorage()
 </script>
 
 <template>
-  <div class="greetings">
-    <!-- now loop over the comments from data.json and pipe them into SingleComment -->
-    <div v-for="comment in commentsData.comments" :key="comment.id">
-      <SingleComment
-        :content="comment.content"
-        :createdAt="comment.createdAt"
-        :score="comment.score"
-        :user="comment.user"
-      />
-      <div v-if="comment.replies.length" class="reply-wrapper">
-        <div v-for="reply in comment.replies" :key="reply.id">
-          <SingleComment
-            :content="reply.content"
-            :createdAt="reply.createdAt"
-            :score="reply.score"
-            :user="reply.user"
-            :replyingTo="reply.replyingTo"
-            class="reply"
-          />
+  <div>
+    <template v-if="loading">Loading..</template>
+    <template v-if="!loading">
+      <div v-for="comment in CommentsLocal?.comments" :key="comment.id">
+        <SingleComment
+          :content="comment.content"
+          :createdAt="comment.createdAt"
+          :score="comment.score"
+          :user="comment.user"
+        />
+        <div v-if="comment.replies.length" class="reply-wrapper">
+          <div v-for="reply in comment.replies" :key="reply.id">
+            <SingleComment
+              :content="reply.content"
+              :createdAt="reply.createdAt"
+              :score="reply.score"
+              :user="reply.user"
+              :replyingTo="reply.replyingTo"
+              class="reply"
+            />
+          </div>
         </div>
       </div>
-    </div>
-    <div class="card">
-      <img :src="commentsData.currentUser.image.webp" alt="" />
-      <p>{{ commentsData.currentUser.username }}</p>
-      <!-- <pre>{{ commentsData }}</pre> -->
-    </div>
+      <div class="card add-comment">
+        <div class="inner-wrapper">
+          <img
+            :src="commentsData.currentUser.image.webp"
+            :alt="commentsData.currentUser.username"
+          />
+          <textarea
+            name="newComment"
+            id="newComment"
+            cols="30"
+            rows="3"
+            placeholder="Add a comment..."
+            resize="none"
+            v-model="newComment.content"
+          ></textarea>
+          <button class="button" @click="addComment">Send</button>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -49,15 +108,47 @@ h3 {
   font-size: 1.2rem;
 }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
+.add-comment {
+  background-color: var(--neutral-white);
+  border-radius: 10px;
+  color: var(--neutral-dark-blue);
+  padding: 16px;
+  margin-block-end: 16px;
+  box-shadow:
+    0 0 10px rgba(0, 0, 0, 0.1),
+    0 4px 4px rgba(0, 0, 0, 0.1);
+  & .inner-wrapper {
+    width: 100%;
+    display: grid;
+    gap: 8px;
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+    & img {
+      aspect-ratio: 1/1;
+      width: 40px;
+      margin: 0 auto;
+    }
+
+    & textarea {
+      border-radius: 10px;
+      padding: 16px;
+      font-family: Rubik, Helvetica, Arial, sans-serif;
+      font-size: 16px;
+      color: var(--neutral-dark-blue);
+      border-color: var(--neutral-light-gray);
+    }
+    & button.button {
+      border-radius: 10px;
+      background-color: var(--primary-moderate-blue);
+      text-transform: uppercase;
+      color: var(--neutral-white);
+      font-weight: 700;
+      max-height: 50px;
+      box-shadow: none;
+      border: none;
+      &:hover {
+        animation: button-hover 800ms 100ms ease-in-out alternate infinite;
+      }
+    }
   }
 }
 
@@ -75,6 +166,48 @@ h3 {
     position: absolute;
     height: 111%;
     translate: -56px -26px;
+  }
+}
+
+.inner-wrapper {
+  @media screen and (max-width: 500px) {
+    grid-template-areas:
+      'meta'
+      'main'
+      'vote';
+    grid-template-columns: auto;
+    & .vote-buttons {
+      /* grid-area: vote;
+      display: flex;
+      flex-direction: row;
+      background-color: var(--neutral-light-gray);
+      border-radius: 10px;
+      justify-content: space-between;
+      width: 85px;
+      margin: 4px 0; */
+      & button {
+        border-color: transparent;
+        border-width: 0px;
+        background-color: transparent;
+        cursor: pointer;
+        box-shadow: none;
+        /* overflow: hidden; */
+        margin: 4px 0;
+        display: block;
+        flex-grow: 1;
+        &:hover img {
+          animation: button-hover 800ms 100ms ease-in-out alternate infinite;
+        }
+      }
+      & span {
+        color: var(--primary-moderate-blue);
+        font-weight: 700;
+      }
+    }
+  }
+  @media screen and (min-width: 500px) {
+    grid-template-areas: 'vote meta button';
+    grid-template-columns: 80px 1fr 150px;
   }
 }
 </style>
--- Interactive comments section/src/components/SingleComment.vue
+++ Interactive comments section/src/components/SingleComment.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="comment">
     <div class="inner-wrapper">
-      <div class="vote-buttons">{{ score }}</div>
+      <div class="vote-buttons">
+        <button @click="increaseVote()">
+          <img src="./../components/images/icon-plus.svg" alt="Vote up" srcset="" /></button
+        ><Transition name="update"
+          ><span class="vote-score" v-if="CommentScore">{{ CommentScore }} </span>
+        </Transition>
+        <button @click="decreaseVote">
+          <img src="./../components/images/icon-minus.svg" alt="Vote down" srcset="" />
+        </button>
+      </div>
       <div class="meta">
         <div class="user">
           <img :src="user.image.webp" :alt="user.username" />
           <p class="user-name">
             {{ user.username }}
-            <span v-if="replyingTo == commentsData.currentUser.username">you</span>
+            <template v-if="user.username === commentsData.currentUser.username">
+              <span class="username-you-tag"
+                >{{ user.username === commentsData.currentUser.username ? 'you' : '' }}
+              </span>
+            </template>
           </p>
 
           <p class="created-at">{{ createdAt }}</p>
         </div>
+        <button @click="increaseVote()" class="reply">
+          <img src="./../components/images/icon-reply.svg" alt="Reply" srcset="" /> Reply
+        </button>
       </div>
       <div class="main">
         <span v-if="replyingTo" class="replying-to">@{{ replyingTo }}</span>
@@ -22,9 +38,10 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import commentsData from './../data.json'
 
-defineProps<{
+const props = defineProps<{
   content: string
   createdAt: string
   user: {
@@ -37,6 +54,15 @@ defineProps<{
   score: number
   replyingTo?: string
 }>()
+
+const CommentScore = ref(props.score)
+
+const increaseVote = () => {
+  CommentScore.value += 1
+}
+const decreaseVote = () => {
+  CommentScore.value -= 1
+}
 </script>
 
 <style scoped>
@@ -53,13 +79,36 @@ defineProps<{
 .inner-wrapper {
   width: 100%;
   display: grid;
-  grid-template-areas:
-    'vote meta'
-    'vote main';
-  grid-template-columns: 80px 1fr;
   gap: 8px;
+
   & .vote-buttons {
     grid-area: vote;
+    display: flex;
+    flex-direction: column;
+    background-color: var(--neutral-light-gray);
+    text-align: center;
+    border-radius: 10px;
+    justify-content: space-between;
+    width: 50px;
+    margin: 0 auto;
+    & button {
+      border-color: transparent;
+      border-width: 0px;
+      background-color: transparent;
+      cursor: pointer;
+      box-shadow: none;
+      /* overflow: hidden; */
+      margin: 2px auto;
+      display: block;
+      flex-grow: 1;
+      &:hover img {
+        animation: button-hover 800ms 100ms ease-in-out alternate infinite;
+      }
+    }
+    & span {
+      color: var(--primary-moderate-blue);
+      font-weight: 700;
+    }
   }
   & .main {
     grid-area: main;
@@ -89,6 +138,99 @@ defineProps<{
         color: var(--neutral-grayish-blue);
       }
     }
+    & span.username-you-tag {
+      color: var(--neutral-white);
+      background-color: var(--primary-moderate-blue);
+      padding: 2px 5px;
+      font-size: 80%;
+      border-radius: 3px;
+    }
+  }
+  .update-enter-active {
+    animation: update 500ms;
+  }
+  .update-leave-active {
+    animation: update 500ms reverse;
+  }
+}
+
+button.reply {
+  background-color: transparent;
+  box-shadow: none;
+  border: none;
+  color: var(--primary-moderate-blue);
+  font-weight: 500;
+}
+.inner-wrapper {
+  @media screen and (max-width: 500px) {
+    grid-template-areas:
+      'meta'
+      'main'
+      'vote';
+    grid-template-columns: auto;
+    & .vote-buttons {
+      grid-area: vote;
+      display: flex;
+      flex-direction: row;
+      background-color: var(--neutral-light-gray);
+      border-radius: 10px;
+      justify-content: space-between;
+      width: 85px;
+      margin: 4px 0;
+      & button {
+        border-color: transparent;
+        border-width: 0px;
+        background-color: transparent;
+        cursor: pointer;
+        box-shadow: none;
+        /* overflow: hidden; */
+        margin: 4px 0;
+        display: block;
+        flex-grow: 1;
+        &:hover img {
+          animation: button-hover 800ms 100ms ease-in-out alternate infinite;
+        }
+      }
+      & span {
+        color: var(--primary-moderate-blue);
+        font-weight: 700;
+      }
+    }
+  }
+  @media screen and (min-width: 500px) {
+    grid-template-areas:
+      'vote meta'
+      'vote main';
+    grid-template-columns: 80px 1fr;
+  }
+}
+@keyframes button-hover {
+  0% {
+    scale: 1;
+    backdrop-filter: blur(0px);
+  }
+  50% {
+    scale: 1.5;
+    backdrop-filter: blur(1px);
+    filter: hue-rotate(180deg);
+  }
+  100% {
+    scale: 1;
+    backdrop-filter: blur(0px);
+    filter: hue-rotate(0deg);
+  }
+}
+@keyframes update {
+  0% {
+    scale: 0.45;
+    opacity: 0;
+  }
+  40% {
+    scale: 1.12;
+  }
+  100% {
+    scale: 1;
+    opacity: 1;
   }
 }
 </style>
